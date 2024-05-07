863aef213cadb62ec04370a96b3618a5d7b8b37d
Simon Rygård <simon.rygard+dev.github@gmail.com>
2024-05-07 20:43:24 +0200

    feat: history endpoints

--- README.md
+++ README.md
@@ -1,11 +1,35 @@
 # keyvaluestore
 
-## Getting a value
+A naive key value store implemented in Go, serving over HTTP.
+
+## Features
+
+- Uses the Go 1.22 "[Enhanced routing patterns](https://tip.golang.org/doc/go1.22)".
+- Best-attempt tries to implement a RESTful API.
+- Uses OpenAPI 3 for API documentation.
+
+## HTTP API
+
+### Getting a value
 
 ```bash
-curl -X GET 'localhost:8080/bar?yo'
+curl -X GET 'localhost:8080/kv?key=simon'
 ```
 
+### Setting a value
+
 ```bash
-curl -X PUT 'localhost:8080/bar' -d '{"key": "yo", "value": "hello"}'
+curl -X PUT 'localhost:8080/kv' -d '{"key": "yo", "value": "hello"}'
+```
+
+### Getting the history
+
+```bash
+curl -X PUT 'localhost:8080/kv/hist?yo'
+```
+
+### Deleting all history
+
+```bash
+curl -X PUT 'localhost:8080/kv/del_hist' -d '{"key": "yo"}'
 ```
--- main.go
+++ main.go
@@ -5,30 +5,63 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+	"time"
 )
 
-type PutRequest struct {
-	Key   string `json:"key"`
-	Value string `json:"value"`
+type entry struct {
+	Value string    `json:"value"`
+	Ts    time.Time `json:"enteredAt"`
 }
 
-type KeyValueStore struct {
-	data map[string]string
+type Store struct {
+	data map[string][]entry
 	mu   sync.Mutex
+}
+
+type HistoryHandler struct {
+	store *Store
+}
+
+func (kv *Store) Put(key, value string) {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	_, ok := kv.data[key]
+	if !ok {
+		kv.data[key] = []entry{{Value: value, Ts: time.Now()}}
+		return
+	}
+
+	kv.data[key] = append(kv.data[key], entry{Value: value, Ts: time.Now()})
+}
+
+func (kv *Store) DeleteAll(key string) {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	_, ok := kv.data[key]
+	if !ok {
+		return
+	}
+
+	kv.data[key] = nil
 }
 
 func main() {
 
-	store := KeyValueStore{
-		data: map[string]string{},
+	store := &Store{
+		data: map[string][]entry{},
 		mu:   sync.Mutex{},
 	}
 
 	http.HandleFunc("/kv", func(w http.ResponseWriter, r *http.Request) {
-
 		switch r.Method {
 		case "PUT":
-			var p PutRequest
+			var p struct {
+				Key   string `json:"key"`
+				Value string `json:"value"`
+			}
+
 			err := json.NewDecoder(r.Body).Decode(&p)
 			if err != nil {
 				http.Error(w, fmt.Sprintf("decode: %v", err), http.StatusBadRequest)
@@ -39,9 +72,7 @@ func main() {
 				http.Error(w, "both key and value must be set", http.StatusBadRequest)
 				return
 			}
-			store.mu.Lock()
-			store.data[p.Key] = p.Value
-			store.mu.Unlock()
+			store.Put(p.Key, p.Value)
 
 			fmt.Fprintf(w, "PUT %v=%v", p.Key, p.Value)
 		case "GET":
@@ -68,13 +99,76 @@ func main() {
 					return
 				}
 
-				fmt.Fprintf(w, "GET %v=%v", k, entry)
+				fmt.Fprintf(w, "GET %v=%v", k, entry[len(entry)-1].Value)
 			}
 		default:
 			fmt.Println(r.Method)
 		}
 	})
 
+	http.Handle("/kv/hist", &HistoryHandler{store: store})
+
+	http.HandleFunc("/kv/del_hist", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "PUT" {
+			http.Error(w, "must be PUT method", http.StatusBadRequest)
+		}
+
+		var d struct {
+			Key string `json:"key"`
+		}
+
+		err := json.NewDecoder(r.Body).Decode(&d)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("decode: %v", err), http.StatusBadRequest)
+			return
+		}
+
+		if d.Key == "" {
+			http.Error(w, "key must be set", http.StatusBadRequest)
+			return
+		}
+
+		store.DeleteAll(d.Key)
+
+		fmt.Fprintf(w, "PUT %v=''", d.Key)
+	})
+
 	fmt.Println("Serving...")
 	http.ListenAndServe(":8080", nil)
+}
+
+func (h *HistoryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "must be GET method", http.StatusBadRequest)
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("parse form: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	if len(r.Form) != 1 {
+		http.Error(w, "Only a single query parameter is supported", http.StatusBadRequest)
+		return
+	}
+
+	for k, v := range r.Form {
+		if len(v) != 1 {
+			http.Error(w, "Only a single value per param is supported", http.StatusBadRequest)
+			return
+		}
+
+		entry, ok := h.store.data[k]
+		if !ok {
+			http.Error(w, fmt.Sprintf("%v does not exist", k), http.StatusBadRequest)
+			return
+		}
+
+		err := json.NewEncoder(w).Encode(entry)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("encode: %v", err), http.StatusInternalServerError)
+			return
+		}
+	}
 }
