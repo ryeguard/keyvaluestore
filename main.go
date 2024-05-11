701157722cd88569d9be67ee035ed92e984ce206
Simon Rygård <simon.rygard+dev.github@gmail.com>
2024-05-11 22:09:46 +0200

    refactor: higher order func for rest of endpoints

--- main.go
+++ main.go
@@ -50,88 +50,67 @@ func main() {
 		mu:   sync.Mutex{},
 	}
 
-	http.HandleFunc("/kv", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case "PUT":
-			var p struct {
-				Key   string `json:"key"`
-				Value string `json:"value"`
-			}
-
-			err := json.NewDecoder(r.Body).Decode(&p)
-			if err != nil {
-				http.Error(w, fmt.Sprintf("decode: %v", err), http.StatusBadRequest)
-				return
-			}
-
-			if p.Key == "" || p.Value == "" {
-				http.Error(w, "both key and value must be set", http.StatusBadRequest)
-				return
-			}
-			store.Put(p.Key, p.Value)
-
-			fmt.Fprintf(w, "PUT %v=%v", p.Key, p.Value)
-		case "GET":
-			err := r.ParseForm()
-			if err != nil {
-				http.Error(w, fmt.Sprintf("parse form: %v", err), http.StatusBadRequest)
-				return
-			}
-
-			if len(r.Form) != 1 {
-				http.Error(w, "Only a single query parameter is supported", http.StatusBadRequest)
-				return
-			}
-
-			for k, v := range r.Form {
-				if len(v) != 1 {
-					http.Error(w, "Only a single value per param is supported", http.StatusBadRequest)
-					return
-				}
-
-				entry, ok := store.data[k]
-				if !ok {
-					http.Error(w, fmt.Sprintf("%v does not exist", k), http.StatusBadRequest)
-					return
-				}
-
-				fmt.Fprintf(w, "GET %v=%v", k, entry[len(entry)-1].Value)
-			}
-		default:
-			fmt.Println(r.Method)
-		}
-	})
-
+	http.HandleFunc("PUT /kv", putHandler(store))
+	http.HandleFunc("GET /kv", getHandler(store))
 	http.HandleFunc("GET /kv/hist/", historyHandler(store))
+	http.HandleFunc("PUT /kv/del_hist", deleteHistoryHandler(store))
 
-	http.HandleFunc("PUT /kv/del_hist", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "PUT" {
-			http.Error(w, "must be PUT method", http.StatusBadRequest)
+	mux := http.NewServeMux()
+	http.ListenAndServe(":8080", mux)
+}
+
+func putHandler(store *Store) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var p struct {
+			Key   string `json:"key"`
+			Value string `json:"value"`
 		}
 
-		var d struct {
-			Key string `json:"key"`
-		}
-
-		err := json.NewDecoder(r.Body).Decode(&d)
+		err := json.NewDecoder(r.Body).Decode(&p)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("decode: %v", err), http.StatusBadRequest)
 			return
 		}
 
-		if d.Key == "" {
-			http.Error(w, "key must be set", http.StatusBadRequest)
+		if p.Key == "" || p.Value == "" {
+			http.Error(w, "both key and value must be set", http.StatusBadRequest)
+			return
+		}
+		store.Put(p.Key, p.Value)
+
+		fmt.Fprintf(w, "PUT %v=%v", p.Key, p.Value)
+	}
+}
+
+func getHandler(store *Store) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("parse form: %v", err), http.StatusBadRequest)
 			return
 		}
 
-		store.DeleteAll(d.Key)
+		if len(r.Form) != 1 {
+			http.Error(w, "Only a single query parameter is supported", http.StatusBadRequest)
+			return
+		}
 
-		fmt.Fprintf(w, "PUT %v=''", d.Key)
-	})
+		for k, v := range r.Form {
+			if len(v) != 1 {
+				http.Error(w, "Only a single value per param is supported", http.StatusBadRequest)
+				return
+			}
 
-	fmt.Println("Serving...")
-	mux := http.NewServeMux()
-	http.ListenAndServe(":8080", mux)
+			entry, ok := store.data[k]
+			if !ok {
+				http.Error(w, fmt.Sprintf("%v does not exist", k), http.StatusBadRequest)
+				return
+			}
+
+			fmt.Fprintf(w, "GET %v=%v", k, entry[len(entry)-1].Value)
+		}
+	}
 }
 
 func historyHandler(store *Store) func(w http.ResponseWriter, r *http.Request) {
@@ -166,5 +145,32 @@ func historyHandler(store *Store) func(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 		}
+	}
+}
+
+func deleteHistoryHandler(store *Store) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "PUT" {
+			http.Error(w, "must be PUT method", http.StatusBadRequest)
+		}
+
+		var d struct {
+			Key string `json:"key"`
+		}
+
+		err := json.NewDecoder(r.Body).Decode(&d)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("decode: %v", err), http.StatusBadRequest)
+			return
+		}
+
+		if d.Key == "" {
+			http.Error(w, "key must be set", http.StatusBadRequest)
+			return
+		}
+
+		store.DeleteAll(d.Key)
+
+		fmt.Fprintf(w, "PUT %v=''", d.Key)
 	}
 }
